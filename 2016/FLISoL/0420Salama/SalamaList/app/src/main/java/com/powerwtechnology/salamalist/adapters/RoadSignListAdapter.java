00ecbc2cc508c76dd461bd17bff3e981b3b5b981
itrjwyss <itrjwyss@gmail.com>
2016-04-22 11:28:19 -0600

    Adding SalamaList Android Proyecto

---
+++ 2016/FLISoL/0420Salama/SalamaList/app/src/main/java/com/powerwtechnology/salamalist/adapters/RoadSignListAdapter.java
@@ -0,0 +1,57 @@
+package com.powerwtechnology.salamalist.adapters;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.powerwtechnology.salamalist.R;
+import com.powerwtechnology.salamalist.objects.RoadSign;
+
+import java.util.List;
+
+/**
+ * Created by mercedeswyss on 16/04/16.
+ */
+public class RoadSignListAdapter extends ArrayAdapter<RoadSign> {
+
+    private LayoutInflater inflater;
+
+    public RoadSignListAdapter(Activity context, List<RoadSign> data){
+        super(context, R.layout.item_road_sign, data);
+        this.inflater = context.getLayoutInflater();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        if(convertView == null){
+            convertView = inflater.inflate(R.layout.item_road_sign, null);
+
+            RoadSign roadSign = getItem(position);
+            ((TextView) convertView.findViewById(R.id.tv_road_sign_name)).setText(roadSign.getRoadSignName());
+            ((TextView) convertView.findViewById(R.id.tv_road_sign_description)).setText(roadSign.getRoadSignDescription());
+
+            ImageView iconView = (ImageView) convertView.findViewById(R.id.iv_road_sign_icon);
+            if(roadSign.getRoadSignType() == RoadSign.RoadSignType.NO_BICYCLES){
+                iconView.setImageResource(R.drawable.no_bicycles);
+            } else if(roadSign.getRoadSignType() == RoadSign.RoadSignType.NO_LEFT){
+                iconView.setImageResource(R.drawable.no_left);
+            } else if(roadSign.getRoadSignType() == RoadSign.RoadSignType.NO_PARKING){
+                iconView.setImageResource(R.drawable.no_parking);
+            } else if(roadSign.getRoadSignType() == RoadSign.RoadSignType.NO_PEDESTRIANS){
+                iconView.setImageResource(R.drawable.no_pedestrians);
+            } else if(roadSign.getRoadSignType() == RoadSign.RoadSignType.NO_RIGHT){
+                iconView.setImageResource(R.drawable.no_right);
+            } else if(roadSign.getRoadSignType() == RoadSign.RoadSignType.NO_TRUCKS){
+                iconView.setImageResource(R.drawable.no_trucks);
+            } else if(roadSign.getRoadSignType() == RoadSign.RoadSignType.NO_U_TURN){
+                iconView.setImageResource(R.drawable.no_u_turn);
+            }
+        }
+        return convertView;
+    }
+
+}
---
+++ 2016/FLISoL/0420Salama/SalamaList/app/src/main/java/com/powerwtechnology/salamalist/objects/RoadSign.java
@@ -0,0 +1,84 @@
+package com.powerwtechnology.salamalist.objects;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by mercedeswyss on 16/04/16.
+ */
+public class RoadSign implements Parcelable {
+
+    public static enum RoadSignType {
+        NO_BICYCLES, NO_LEFT, NO_PARKING, NO_PEDESTRIANS, NO_RIGHT, NO_TRUCKS, NO_U_TURN
+    }
+
+    private String roadSignName;
+
+    private String roadSignDescription;
+
+    private RoadSignType roadSignType;
+
+    public RoadSign(){}
+
+    public RoadSign(String roadSignName, String roadSignDescription, RoadSignType roadSignType){
+        this.roadSignName = roadSignName;
+        this.roadSignDescription = roadSignDescription;
+        this.roadSignType = roadSignType;
+    }
+
+    protected RoadSign(Parcel in) {
+        roadSignName = in.readString();
+        roadSignDescription = in.readString();
+        roadSignType = (RoadSignType) in.readValue(RoadSignType.class.getClassLoader());
+    }
+
+    public String getRoadSignName() {
+        return roadSignName;
+    }
+
+    public void setRoadSignName(String roadSignName) {
+        this.roadSignName = roadSignName;
+    }
+
+    public String getRoadSignDescription() {
+        return roadSignDescription;
+    }
+
+    public void setRoadSignDescription(String roadSignDescription) {
+        this.roadSignDescription = roadSignDescription;
+    }
+
+    public RoadSignType getRoadSignType() {
+        return roadSignType;
+    }
+
+    public void setRoadSignType(RoadSignType roadSignType) {
+        this.roadSignType = roadSignType;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(roadSignName);
+        dest.writeString(roadSignDescription);
+        dest.writeValue(roadSignType);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Creator<RoadSign> CREATOR = new Creator<RoadSign>() {
+        @Override
+        public RoadSign createFromParcel(Parcel in) {
+            return new RoadSign(in);
+        }
+
+        @Override
+        public RoadSign[] newArray(int size) {
+            return new RoadSign[size];
+        }
+    };
+
+}
